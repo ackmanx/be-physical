45e2ff5b0c6d84ddf2332fdc01e5a04e7ef154eb
ackmanx <ackmanx@users.noreply.github.com>
2023-10-17 22:09:40 -0500

    Stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,58 @@
 <style>
+  section {
+    display: flex;
+  }
+
+  input {
+    margin-top: 8px;
+    margin-right: 16px;
+    transform: scale(1.25);
+  }
 </style>
 
 <section>
-  <h2>Seated scapular retractions</h2>
-  <p>6 reps</p>
-  <p>Pinch shoulder blades together. Don't forget about mid-back pinching too.</p>
+  <div>
+    <input type="checkbox" />
+  </div>
+  <div>
+    <h2>Seated scapular retractions</h2>
+    <p>6 reps</p>
+    <p>Pinch shoulder blades together. Don't forget about mid-back pinching too.</p>
+  </div>
 </section>
+
 <section>
-  <h2>Stabilized head flexion</h2>
-  <p>5 reps</p>
-  <p>Tilt head downwards, then kind of back and up, in one fluid motion.</p>
+  <div>
+    <input type="checkbox" />
+  </div>
+  <div>
+    <h2>Stabilized head flexion</h2>
+    <p>5 reps</p>
+    <p>Tilt head downwards, then kind of back and up, in one fluid motion.</p>
+  </div>
 </section>
+
 <section>
-  <h2>Axial extension of the neck</h2>
-  <p>5 reps</p>
-  <p>Look straight ahead and slide head backwards.</p>
+  <div>
+    <input type="checkbox" />
+  </div>
+  <div>
+    <h2>Axial extension of the neck</h2>
+    <p>5 reps</p>
+    <p>Look straight ahead and slide head backwards.</p>
+  </div>
 </section>
+
 <section>
-  <h2>TMJ rotation</h2>
-  <p>6 reps, 4 sets</p>
-  <p>One finger in front of ear and another gently pushing chin downward. TMJ should not protrude outward on finger.</p>
+  <div>
+    <input type="checkbox" />
+  </div>
+  <div>
+    <h2>TMJ rotation</h2>
+    <p>6 reps, 4 sets</p>
+    <p>
+      One finger in front of ear and another gently pushing chin downward. TMJ should not protrude
+      outward on finger.
+    </p>
+  </div>
 </section>
