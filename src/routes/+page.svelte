100b4ae347641dff19ac77918ecc3aaaf9269847
ackmanx <ackmanx@users.noreply.github.com>
2023-10-17 21:41:45 -0500

    Exercises

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+</style>
+
+<section>
+  <h2>Seated scapular retractions</h2>
+  <p>6 reps</p>
+  <p>Pinch shoulder blades together. Don't forget about mid-back pinching too.</p>
+</section>
+<section>
+  <h2>Stabilized head flexion</h2>
+  <p>5 reps</p>
+  <p>Tilt head downwards, then kind of back and up, in one fluid motion.</p>
+</section>
+<section>
+  <h2>Axial extension of the neck</h2>
+  <p>5 reps</p>
+  <p>Look straight ahead and slide head backwards.</p>
+</section>
+<section>
+  <h2>TMJ rotation</h2>
+  <p>6 reps, 4 sets</p>
+  <p>One finger in front of ear and another gently pushing chin downward. TMJ should not protrude outward on finger.</p>
+</section>
