8da4f3d629de088f4d01a5c348493b34fb30e7cc
ackmanx <ackmanx@users.noreply.github.com>
2023-10-17 22:24:09 -0500

    Stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,31 @@
-<style>
-  section {
-    display: flex;
-  }
+<script>
+  import Exercise from './Exercise.svelte'
+</script>
 
-  input {
-    margin-top: 8px;
-    margin-right: 16px;
-    transform: scale(1.25);
-  }
+<style>
 </style>
 
-<section>
-  <div>
-    <input type="checkbox" />
-  </div>
-  <div>
-    <h2>Seated scapular retractions</h2>
-    <p>6 reps</p>
-    <p>Pinch shoulder blades together. Don't forget about mid-back pinching too.</p>
-  </div>
-</section>
+<Exercise
+  name="Seated scapular retractions"
+  reps="6"
+  description="Pinch shoulder blades together. Don't forget about mid-back pinching too."
+/>
 
-<section>
-  <div>
-    <input type="checkbox" />
-  </div>
-  <div>
-    <h2>Stabilized head flexion</h2>
-    <p>5 reps</p>
-    <p>Tilt head downwards, then kind of back and up, in one fluid motion.</p>
-  </div>
-</section>
+<Exercise
+  name="Stabilized head flexion"
+  reps="5"
+  description="Tilt head downwards, then kind of back and up, in one fluid motion."
+/>
 
-<section>
-  <div>
-    <input type="checkbox" />
-  </div>
-  <div>
-    <h2>Axial extension of the neck</h2>
-    <p>5 reps</p>
-    <p>Look straight ahead and slide head backwards.</p>
-  </div>
-</section>
+<Exercise
+  name="Axial extension of the neck"
+  reps="5"
+  description="Look straight ahead and slide head backwards."
+/>
 
-<section>
-  <div>
-    <input type="checkbox" />
-  </div>
-  <div>
-    <h2>TMJ rotation</h2>
-    <p>6 reps, 4 sets</p>
-    <p>
-      One finger in front of ear and another gently pushing chin downward. TMJ should not protrude
-      outward on finger.
-    </p>
-  </div>
-</section>
+<Exercise
+  name="TMJ rotation"
+  reps="6"
+  sets="4"
+  description="One finger in front of ear and another gently pushing chin downward. TMJ should not protrude outward on finger too."
+/>
---
+++ src/routes/Exercise.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let name
+    export let reps
+    export let sets
+    export let description
+</script>
+
+<style>
+  section {
+    display: flex;
+  }
+
+  input {
+    margin-top: 8px;
+    margin-right: 16px;
+    transform: scale(1.25);
+  }
+</style>
+
+<section>
+    <div>
+        <input type="checkbox" />
+    </div>
+    <div>
+        <h2>{name}</h2>
+        <p>{reps} reps, {sets} sets</p>
+        <p>{description}</p>
+    </div>
+</section>
